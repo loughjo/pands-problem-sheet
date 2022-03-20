0fe52995dec91762688a0add82721661cf61464b
John Loughnane <john.loughnane@gmail.com>
2022-03-20 17:30:46 +0000

    Make changes to the code for weekly task 03 - secondstring.py

--- secondstring.py
+++ secondstring.py
@@ -1,10 +1,15 @@
-# 
-# Author John Loughnane
 # Write a program that asks a user to input a string and outputs every second letter in reverse order.
+# Author : John Loughnane
 
-# Use the input method to enter in a sentence and assign it to the variable sentence
+# https://stackoverflow.com/questions/58886774/python-how-do-i-reverse-every-other-string-in-a-list (reverse a string)
+# https://www.w3schools.com/python/python_howto_reverse_string.asp
+
+# Use the input method to enter in a sentence and assign it to a String variable called sentence
 sentence = input("Please enter a sentence : ")
 
-# Takes in a string and outputs every second letter in reverse order
-a = sentence[::-2]
-print(a)
+# Takes in a string and use the slice function to output every second letter in reverse order.
+# To do that we need to sue the syntax = [::-2] where -2 will step backwards and output every second character 
+reverseSentence = sentence[::-2]
+
+# print out the contents of the reverseSentence string variable.
+print(reverseSentence)
