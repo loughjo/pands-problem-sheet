72604a79727a3a3568da428dc80c528c2d2b2b82
John Loughnane <john.loughnane@gmail.com>
2022-03-20 16:36:51 +0000

    Make changes to the code for weekly task 02 - bmi.py

--- bmi.py
+++ bmi.py
@@ -1,12 +1,18 @@
 # Program to calculate somebody's Body Mass index (BMI)
 # Author: John Loughnane
 
+# https://www.tutorialspoint.com/How-to-round-down-to-2-decimals-a-float-using-Python
+# https://www.youtube.com/watch?v=PpuiO6WJxic = bmi formula
+
 # Use input function to accept inputs from user for weight and height
-weight = input('Enter your weight in kilograms : ')
-height = input('Enter your height in centimetres : ')
+# Since input function holds a string we need to convert height and weight to an integer so we can calaculate the BMI.
+weight = int(input('Enter your weight in kilograms : '))
+height = int(input('Enter your height in centimetres : '))
 
 # Calculate the BMI from the height and weight that was entered
-bmi = (int(weight))/(int(height)/100)
-bmi = bmi / (int(height)/100)
+# Use the round function to round the bmi to 2 decimal places
+# formula => weight(kg)/ height (m2) - divide height by 100 to covert to metres.
+bmi = round(weight/((height/100)**2),2)
 
+# Print out the BMI a nswer that was calculated
 print('The BMI is (kg/m*2): ' + str(bmi))
