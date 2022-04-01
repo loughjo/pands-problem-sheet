6c007da84bf1f063d85baeb30ca81de2d8e572e7
John Loughnane <john.loughnane@gmail.com>
2022-04-01 22:49:16 +0100

    Updates made to squareroot.py version 3

--- squareroot.py
+++ squareroot.py
@@ -1,20 +1,29 @@
-
+# A program that takes a positive floating-point number as input and outputs an approximation of its square root.
 # Author : John Loughnane
 
-num = int(input("Enter a number : "))
+# 
+number = float(input("Please enter a positive number: "))
 
-squareroot = 1
-y = 1
+# Two variables defined, variable count is use in the IF statement when count equals 0 
+# we will run the IF statement or ELSE we will do something different. 
+# The approximation variable is assigned 
+count = 0
+approximation = 0
 
-while y**2 != num:
-    num1 = (squareroot**2)-2
-    print()
-    num2 = 2*squareroot
-    print("num 2 :",)
-    num3 = squareroot - (num1/num2)
-    squareroot = num3
-    y = num3
-    
-    print("internal",squareroot)
+# While the variable number which is entered is NOT EQUAL to approximation * approximation continue with the IF/ELSE statment
+# The approxiamation variable is our estimation of the SQUARE ROOT so if this result is EQUAL to number we stop.
+# We round the approximation * approximation to 1 decimal place. 
+while number != (round(approximation*approximation,1)) :
+    if count == 0:
+        approximation = (number + (number/number))/2
+        count +=1
+        #print(approximation)
+    else:
+        approximation = (approximation + (number/approximation))/2
+        #count += 1
+        #print(approximation)
 
-print(squareroot)
+# We round float number assigned to the approximation variable to one decimal place as this is required for the answer.
+# Then we print the approximation calculated 
+approximation = round(approximation,1)
+print('The square root of {} is approx. {}.'.format(number, approximation))
